9b7d87c0dc54413e52ab27aceeb270c51070b2bc
Sajeevan R S <rssajeevan1010@gmail.com>
2023-10-20 14:34:29 +0530

    commit 2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
-console.log('Server...');
+const express = require('express');
+const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: ['http://localhost:8000', 'http://localhost:5173'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
